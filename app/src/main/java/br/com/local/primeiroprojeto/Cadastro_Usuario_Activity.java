273db9619c524b62211ba300cd3b7863465b26b3
Ecthor <EcthorSilva@outlook.com>
2021-04-05 20:27:19 -0300

    Update 05/04

--- app/src/main/java/br/com/local/primeiroprojeto/Cadastro_Usuario_Activity.java
+++ app/src/main/java/br/com/local/primeiroprojeto/Cadastro_Usuario_Activity.java
@@ -2,13 +2,37 @@ package br.com.local.primeiroprojeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cadastro_Usuario_Activity extends AppCompatActivity {
+
+    // declarar os componente que irei acessar pelo id do XML. vão ser componentes globais
+    Button btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_usuario_layout);
+
+        // vai executar o layout
+        setContentView(R.layout.cadastro_usuario_layout);
+
+        // realizar a ligação do componente que esta no java com o componente no XML
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+        // executando uma ação de click
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //voltar para a janela de login, abrindo essa janela
+                startActivity(new Intent(getApplicationContext(), Login_Activity.class));
+
+                // fechando a janela
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/com/local/primeiroprojeto/Login_Activity.java
+++ app/src/main/java/br/com/local/primeiroprojeto/Login_Activity.java
@@ -2,7 +2,9 @@ package br.com.local.primeiroprojeto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+    }
+
+    public void loginToPrincipal(View view) {
+        // mostra uma mensagem para o usuario
+        //Toast.makeText(getApplicationContext(), "Carregando", Toast.LENGTH_LONG ).show();
+
+        //abrir janela do main activity
+
+        Intent abrirJanela = new Intent(getApplicationContext(), Cadastro_Usuario_Activity.class);
+        startActivity(abrirJanela);
     }
 }
